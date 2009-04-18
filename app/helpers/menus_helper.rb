0dad050dd17df2b8789f18acb9b0cd69540599be
wangxw <caryl@126.com>
2009-04-18 23:19:11 +0800

    fix a bug

--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -1,6 +1,6 @@
 module MenusHelper
   def granted_menus_for(user)
-    htmls = user.cached_granted_menus.each.map{|m|content_tag :li, m}
+    htmls = user.cached_granted_menus.map{|m|content_tag :li, m}
     content_tag :ul, htmls.join('')
   end
 end
