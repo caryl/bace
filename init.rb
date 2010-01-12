b05ba4051d24baef1aec7f516a8faee4bcfdffae
caryl <xianwei.wang@gmail.com>
2010-01-12 15:33:56 +0800

    update init.rb

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ config.active_record.observers = :bace_cache_observer
 config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
 
 config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
-config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+config.gem "factory_girl", :source => "http://gemcutter.org"
 
 ActionController::Base.send :include, BaceController
 
